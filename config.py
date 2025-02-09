98ac9c825affbb11e9f6ca7f98b53e4747227fbb
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-02-09 03:17:19 +0200

    [BACKEND] added config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+from handymatt.wsl_paths import convert_to_wsl_path
+
+PREVIEW_MEDIA_DIR = convert_to_wsl_path(r'A:\WhisperaHQ\MyPrograms\MyApplications\CandyPopApp\Frontend\media')
+CUSTOM_THUMBS_DIR = '...'
+DB_PATH = 'data/app.db'
+GIFS_DIR = convert_to_wsl_path(r'A:\Whispera\gifsFromCollection')
+
+SCENE_FILENAME_FORMATS = [
+    '{sort_performers} - {studio:S} - [{year:d}];opt [{date_released:%Y-%m-%d}];opt [{line:S}];opt {scene_title} [{mention_performers:D}];opt {{{video_id:S}}};opt [id=={id:S}];opt',
+    '[{studio:S}] [{year:d}];opt [{date_released:%Y-%m-%d}];opt [{line:S}];opt {scene_title} [{mention_performers:D}];opt',
+    '{sort_performers} [{jave_code:S}] [{date_released:%Y-%m-%d}];opt [{studio:S}];opt {scene_title}',
+    '[{jav_code:S}]',
+    '{sort_performers} - {scene_title}',
+    '{scene_title}',
+]
