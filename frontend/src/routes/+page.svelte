101444a200f9934d4e1350249b8c94331e091f71
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-02-16 15:58:41 +0200

    [frontend] added reverse proxy to backend server

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+    console.log("Hey monkeyboy");
+
+    fetch('/api/hello')
+        .then(res => {
+            console.log(res);
+            return res.json()
+        }).then(data => {
+            console.log(data);
+        }).catch(err => {
+            console.error(err);
+        })
+</script>
+
 <h1>Welcome to the Home Page</h1>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'^/(.*)': { // proxy all requests
+				target: 'http://localhost:8000', // Backend server
+				changeOrigin: true,
+				rewrite: (path) => path, // Forward the full path
+			},
+		},
+	},
 });
