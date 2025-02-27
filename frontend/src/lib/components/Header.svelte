a639dfd0ea07bce287d146c58a78c705c6e51f2d
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-02-27 22:52:49 +0200

    [frontend] added footer, nav items

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,20 +1,47 @@
+
+<!-- SCRIPT ----------------------------------------------------------------------------------------------------------->
 <script>
-    import { page } from "$app/stores";
+    import { page } from "$app/stores"; // TODO: figure this shit out
 </script>
 
+
+<!-- HTML ------------------------------------------------------------------------------------------------------------->
 <nav>
-    <a href="/"         class:active={$page.url.pathname === "/"}>Home</a>
-    <a href="/about"    class:active={$page.url.pathname === "/about"}>About</a>
-    <a href="/contact"  class:active={$page.url.pathname === "/contact"}>Contact</a>
+
+    <span>
+        <a href="/"         class:active={$page.url.pathname === "/"}>
+            <div>CandyPop</div>
+            <div>Video</div>
+        </a>
+    </span>
+
+    <span>
+        <a href="/"         class:active={$page.url.pathname === "/"}>Home</a>
+        <a href="/search"    class:active={$page.url.pathname === "/search"}>search</a>
+        <a href="/contact"  class:active={$page.url.pathname === "/contact"}>catalogue</a>
+        <a href="/"  class:active={$page.url.pathname === "/"}>other</a>
+        <a href="/dashboard"  class:active={$page.url.pathname === "/dashboard"}>Dashboard</a>
+
+    </span>
+    
 </nav>
 
+
+<!-- STYLES ----------------------------------------------------------------------------------------------------------->
 <style>
     nav {
         display: flex;
+        background-color: #f0f0f0;
+        justify-content: space-between;
+    }
+    
+    span {
+        display: flex;
         gap: 1rem;
         padding: 1rem;
-        background-color: #f0f0f0;
+        background: green;
     }
+
     .active {
         font-weight: bold;
     }
