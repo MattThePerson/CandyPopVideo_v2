7587af87474fe685b2aeb4dde3a9b5238dda6cac
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-07-23 19:13:42 +0300

    Modified run.sh to create venv if needed

--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -8,20 +8,39 @@ BACKEND_PORT=8000
 TOOLS_DIR="$(dirname "$(readlink -f "$0")")"
 cd "$TOOLS_DIR/.."
 
-# BUILD FRONTEND
-# cd frontend
-# echo "[NPM] Building frontend"
-# npm run build
-# cd ..
 
-# CHECK VENV
-if [ ! -d ".venv" ]; then
-    echo "[VENV] No venv created, run 'tools/install.sh'"
-    exit 1
+# Parse arguments
+forward_args=()
+REINSTALL_VENV=0
+export DEV_MODE="0"
+
+echo "args: $@"
+
+for arg in "$@"; do
+  if [[ "$arg" == "--reinstall-venv" ]]; then
+    echo "[.sh] Captured '--reinstall-venv' command"
+    REINSTALL_VENV=1
+  elif [[ "$arg" == "--dev" ]]; then
+    echo "[bash] Captured '--dev' command"
+    export DEV_MODE="1"
+  else
+    forward_args+=("$arg")
+  fi
+done
+
+
+
+# Create venv if needed
+if [ ! -d ".venv" ] || [ $REINSTALL_VENV -eq 1 ]; then
+    echo "[bash] Installing local python environment (.venv) ..."
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt
+    echo "[bash] Done! Local environment created."
 fi
 
-# START
-echo "[START] Starting uvicorn on port $BACKEND_PORT and extra args: '$@'"
-./.venv/bin/uvicorn main:app --host 0.0.0.0 --workers 1 --port $BACKEND_PORT "$@" # Unsure about using "" around the $@
 
-# ./.venv/bin/python main.py "$@"
+# START
+echo "[bash] Starting uvicorn on port "$BACKEND_PORT" with extra args: '"${forward_args[@]}"'"
+./.venv/bin/uvicorn main:app --host 0.0.0.0 --workers 1 --port $BACKEND_PORT "${forward_args[@]}"
